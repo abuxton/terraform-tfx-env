81baa45a8b9d62439c95e9b01c9fdfa416a824ea
Adam Buxton <buxton.adam@gmail.com>
2022-02-11 20:46:20 +0000

    update example outputs

--- tests/default/outputs.tf
+++ tests/default/outputs.tf
@@ -2,11 +2,11 @@
 output "tfx-env-map" {
   value = module.env.tfx-env-map
 }
-output "tfx-env-tfe_run_id" {
-	# example from map
+output "tfx-env-map_tfe_run_id" {
+  # example from map
   value = module.env.tfx-env-map.tfe_run_id
 }
 output "tfx-env-tfe_run_id" {
-	# example from module output
+  # example from module output
   value = module.env.tfx-env-tfe_run_id
 }
